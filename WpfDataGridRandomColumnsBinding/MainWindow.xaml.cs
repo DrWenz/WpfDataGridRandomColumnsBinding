3b401a7ce2a5341775af55c3f7830ca333072b7e
Markus Wenzl <markus.wenzl@mmw-solutions.com>
2019-12-10 22:15:47 +0100

    Projektdateien hinzufügen.

---
+++ WpfDataGridRandomColumnsBinding/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDataGridRandomColumnsBinding
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<DataGridColumn> PropRandomColumns;
+        private ObservableCollection<TestClass> PropTestDataSource;
+        private Random rnd;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<DataGridColumn> RandomColumns
+        {
+            get => PropRandomColumns;
+            set
+            {
+                PropRandomColumns = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<TestClass> TestDataSource
+        {
+            get => PropTestDataSource;
+            set
+            {
+                PropTestDataSource = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand CreateRandomColumnsCommand => new RelayCommand(CreateRandomColumns);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            rnd = new Random();
+
+
+            this.DataContext = this;
+        }
+
+        private void CreateRandomColumns()
+        {
+            ObservableCollection<TestClass> data = new ObservableCollection<TestClass>();
+            for (int i = 0; i < rnd.Next(20,30); i++)
+            {
+                data.Add(new TestClass("hallo " + i, "irgendwas" + i, "jihaaa" + i));
+            }
+
+            ObservableCollection<DataGridColumn> columns = new ObservableCollection<DataGridColumn>();
+            for (int i = 0; i < rnd.Next(1,12); i++)
+            {
+                columns.Add(new DataGridTextColumn()
+                {
+                    Header = "Erste Spalte",
+                    Binding = new Binding("Property1")
+                });
+            }
+
+
+            RandomColumns = columns;
+            TestDataSource = data;
+        }
+    }
+}
---
+++ WpfDataGridRandomColumnsBinding/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataGridRandomColumnsBinding
+{
+    public class TestClass
+    {
+        public string Property1 { get; }
+        public string Property2 { get; }
+        public string Property3 { get; }
+
+        public TestClass(string property1, string property2, string property3)
+        {
+            Property1 = property1;
+            Property2 = property2;
+            Property3 = property3;
+        }
+    }
+}
